b3f615f64c02a8e7500135d1c197d157af23eead
Mayur Mahadev Chavan <100504440+Sudo-Mayur@users.noreply.github.com>
2022-03-09 15:09:17 +0530

    Delete ConsoleApp4 directory

--- ConsoleApp4/ConsoleApp4/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("xsadsadaf");
-Console.WriteLine("Mayur chavan");
-Console.WriteLine("Jahanvi");
-Console.ReadLine();
